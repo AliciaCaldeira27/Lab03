e87bca6b6d818589f5d8f3ebf4e636cde3ea6093
aliciacaldeira42@gmail.com <aliciacaldeira27@gmail.com>
2025-10-25 16:11:39 -0300

    Atualizando dependências e configuração do Jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,8 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',        // Usar o ts-jest para compilar os testes TypeScript
+  testEnvironment: 'node',  // Definir o ambiente de teste para Node.js
 };
+
+export default config;
