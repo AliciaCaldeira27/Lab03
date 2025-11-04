22faa6b802ac51e06ad7e80b5898cfab918f497a
AliciaCaldeira27 <aliciacaldeira42@gmail.com>
2025-11-04 08:55:58 -0400

    Update and rename jest.config.ts to jest.config.cts

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from '@jest/types';
-
-const config: Config.InitialOptions = {
-  preset: 'ts-jest',        // Usar o ts-jest para compilar os testes TypeScript
-  testEnvironment: 'node',  // Definir o ambiente de teste para Node.js
-};
-
-export default config;
