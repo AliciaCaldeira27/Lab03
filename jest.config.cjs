14236e4693074a2ea3f762c80a9a77bfa6e2658e
AliciaCaldeira27 <aliciacaldeira42@gmail.com>
2025-11-04 09:12:57 -0400

    Update and rename jest.config.cts to jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,13 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: ['.ts'],
   testEnvironment: 'node',
- 
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
     '^.+\\.m?[tj]sx?$': ['ts-jest', { useESM: true }],
   },
- 
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
